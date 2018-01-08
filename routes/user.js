d7443a7d6ea254db3c2f6339474b3abc69a7e508
AcarMeel <acarmeel@gmail.com>
2018-01-07 20:20:04 -0600

    crud finished

--- routes/user.js
+++ routes/user.js
@@ -58,4 +58,62 @@ app.post('/', (req, res) => {
     });
 });
 
+//====================================
+//   Update user
+//====================================
+app.put('/:id', (req, res) => {
+    var id = req.params.id;
+    var body = req.body;
+
+    User.findById(id, (err, foundUser) => {
+        if(err || !foundUser) {
+            return res.status(500).json({
+                ok: false,
+                msg: 'Error finding the user',
+                errors: err
+            });
+        }
+        // Assign new values
+        foundUser.name = body.name;
+        foundUser.email = body.email;
+        foundUser.role = body.role;
+        // Save new values
+        foundUser.save((err, savedUser) => {
+            if(err) {
+                return res.status(400).json({
+                    ok: false,
+                    msg: 'Error updating the user',
+                    errors: err
+                });
+            }
+            res.status(200).json({
+                ok: true,
+                msg: 'User was updated successfully',
+                data: savedUser
+            });
+        });
+    });
+});
+
+//====================================
+//   Delete user
+//====================================
+app.delete('/:id', (req, res) => {
+    var id = req.params.id;
+    User.findByIdAndRemove(id, (err, removedUser) => {
+        if(err || !removedUser) {
+            return res.status(500).json({
+                ok: false,
+                msg: 'Error deleting the user',
+                errors: err
+            });
+        }
+        res.status(200).json({
+            ok: true,
+            msg: 'User was removed successfully',
+            data: removedUser
+        });
+    });
+});
+
 module.exports = app;
