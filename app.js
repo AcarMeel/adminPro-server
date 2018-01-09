70e979a02d881a7209eb27e34e1fef8b8dae4e8e
acarmeel <pxq@akc.org>
2018-01-09 11:42:07 -0600

    added routes

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ var appRoutes = require('./routes/app');
 var userRoutes = require('./routes/user');
 var loginRoutes = require('./routes/login');
 var hospitalRoutes = require('./routes/hospital');
+var doctorRoutes = require('./routes/doctor');
+var searchRoutes = require('./routes/search');
 
 // App declarations
 var app = express();
@@ -27,7 +29,9 @@ mongoose.connection.openUri('mongodb://localhost:27017/hospitalAdminPro', (err, 
 // App Routes
 app.use('/user', userRoutes);
 app.use('/hospital', hospitalRoutes);
+app.use('/doctor', doctorRoutes);
 app.use('/login', loginRoutes);
+app.use('/search', searchRoutes);
 app.use('/', appRoutes);
 
 app.listen(PORT, () => {
