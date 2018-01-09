c897a7460b1f79da81afe9b474754888e941b0d4
acarmeel <pxq@akc.org>
2018-01-09 14:27:26 -0600

    search all

--- routes/search.js
+++ routes/search.js
@@ -9,7 +9,62 @@ var User = require('../models/user');
 app.get('/all/:filter', (req, res, next) => {
     var filter = req.params.filter;
     var regex = new RegExp(filter, 'i');
-    res.send('Hello');
+
+    Promise.all([
+        findHospitals(filter, regex),
+        findDoctors(filter, regex),
+        findUsers(filter, regex)
+    ])
+    .then(results => {
+        res.status(200).json({
+            ok: true,
+            hospitals: results[0],
+            doctors: results[1],
+            users: results[2] 
+        });
+    })
 });
+
+function findHospitals(filter, regex) {
+    return new Promise( (resolve, reject) => {
+     Hospital.find({ name: regex}) 
+        .populate('user', 'name email')
+        .exec((err, hospitals) => {
+            if(err) {
+                reject('Error ocurred while retrieving Hospitals data');
+            } else {
+                resolve(hospitals);
+            }
+     });
+    });
+}
+
+function findDoctors(filter, regex) {
+    return new Promise( (resolve, reject) => {
+     Doctor.find({ name: regex})
+     .populate('user', 'name email')
+     .populate('hospital', 'name')
+      .exec((err, doctors) => {
+         if(err) {
+             reject('Error ocurred while retrieving Doctors data');
+         } else {
+             resolve(doctors);
+         }
+     });
+    });
+}
+
+function findUsers(filter, regex) {
+    return new Promise( (resolve, reject) => {
+     User.find({}, 'name email').or([{ 'name': regex}, {'email': regex}]) 
+         .exec((err, users) => {
+            if(err) {
+                reject('Error ocurred while retrieving Users data');
+            } else {
+                resolve(users);
+            }
+        });
+    });
+}
 
 module.exports = app;
